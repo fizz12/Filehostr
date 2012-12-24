db51fc3b44c0ce4d23a5917e21a863169721061e
fizz12 <michaelv000@gmail.com>
2012-12-23 21:29:52 -0600

    minor frontend edits

--- inc/footer.php
+++ inc/footer.php
@@ -25,9 +25,9 @@
 			Developed by <a href="http://github.com/fizz12/uploader" target="_BLANK">fizz12</a> &copy; 2012 <?php echo NAME;?>.
 		</div>
 		<div class="span5 offset3 license">
-			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/" target="_BLANK"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a>
-			<br />
 			This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/" target="_BLANK">Creative Commons Attribution 3.0 Unported License</a>.
+			<br />
+			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/" target="_BLANK"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/88x31.png" /></a>
 		</div>
 	</div>
 </div>
--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
-<title>Uploader | <?php echo $pagetitle;?></title>
+<title><?php echo NAME;?> | <?php echo $pagetitle;?></title>
 <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
 <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
 <script src="http://code.jquery.com/jquery.min.js"></script>
